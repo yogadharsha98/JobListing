9bc4c99eb33e1b11fc2bacf7f1387718faa04061
Yogeswaran Yogakrishanthi <127289877+yogadharsha98@users.noreply.github.com>
2025-08-12 13:19:03 +0530

    Update vite.config.js
    url updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/api',
+        target: 'https://job-listing-backend-ci8t.onrender.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
